a7ebc9fa35460bf174738808d64e4fcf4fdfe421
flaraloza97 <flaraloza97@penguin>
2020-09-23 00:24:42 -0500

    add c code

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * _islower - check if c is lowercase
+ * @c: string
+ * Return: 1 if lowercase, 0 if not
+ */
+int _islower(int c)
+{
+return (c >= 97 && c <=122);
+}
+
+ 
