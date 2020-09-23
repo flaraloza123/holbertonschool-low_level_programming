d9711d4ec96911685dd611a82cfa199d656792ab
flaraloza97 <flaraloza97@penguin>
2020-09-23 00:38:32 -0500

    edit code

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "holbeton.c"
+
+/**
+ * _isalpha - checks to see if c is a letter
+ * @c: string
+ * Return: 1 if c is a letter, 0 if not
+ */
+
+int _isalpha(int c)
+{
+if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+{
+return (1);
+}
+return (0);
+}
