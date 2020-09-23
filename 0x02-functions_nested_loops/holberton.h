8a4e9ad33cf1551b7a0b849946d7922c6181e26e
flaraloza97 <flaraloza97@penguin>
2020-09-22 23:22:11 -0500

    edits

---
+++ 0x02-functions_nested_loops/holberton.h
@@ -0,0 +1,56 @@
+/**
+ * _putchar - putchar
+ */
+int _puthar(char c);
+
+/**
+ * print alphabet in lowercase
+ */
+void print_alphabet(void);
+
+/**
+ * print alphabet ten times
+ */
+void print_alphabet_x10(void);
+
+/**
+ * check for lowercase character
+ */
+int _islower(int c);
+
+/**
+ * check for alphabetic character
+ */
+int print_sign(int n);
+
+/**
+ * compute absolute value of integer
+ */
+int _abs(int);
+
+/**
+ * print last digit of a number
+ */
+int print_last_digit(int);
+
+/**
+ * prints every minute of the day of Jack Bauer, starting fromm 00:00 to 23:59
+ */
+void jack_bauer(void);
+
+/**
+ * 9-times_table.c
+ */
+void times_table(void);
+
+/**
+ * 10-add.c
+ */
+int add(int, int);
+
+/**
+ * 11-print_to_98.c
+ */
+void print_to_98(int);
+
+#endif
