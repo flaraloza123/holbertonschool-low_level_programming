2d171a5850ab37083a34ff980582b0cc7037e487
flaraloza97 <flaraloza97@penguin>
2020-09-23 23:14:24 -0500

    made edit to code

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * add - writes a function that adds two integers
+ * @x: integer
+ * @t: integer
+ * Return: result of added integers
+ */
+int add(int x, int t)
+{
+return (x + t);
+}
