6e01e2ce689ba9efd6cdf0c958c9357c196b3fb9
flaraloza97 <flaraloza97@penguin>
2020-09-24 18:54:35 -0500

    add all files

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+/**
+ * _isupper - will check in ASCII if input is an uppercase letter
+ * @c: integer
+ * Return: 1 if true, 0 if false
+ */
+int _isupper(int c)
+{
+return (c >= 65 && c <= 90);
+}
