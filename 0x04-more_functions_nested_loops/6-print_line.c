e6a796bbccbcd2d5676d9afd590767720df923a0
flaraloza97 <flaraloza97@penguin>
2020-09-24 22:56:03 -0500

    add code

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,14 +2,14 @@
 
 /**
  * print_line - print line of variable length
- * @n: length of line 
+ * @n: length of line
  */
 void print_line(int n)
 {
-  int i;
+int i;
 
-  if (n > 0)
-    for (i = 0; i < n; i++)
-      _putchar('_')
-	_putchar('\n');
+if (n > 0)
+for (i = 0; i < n; i++)
+_putchar('_')
+_putchar('\n');
 }
