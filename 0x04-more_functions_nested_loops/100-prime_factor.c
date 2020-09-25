1e34741fc15fb50c234b0176840bcbf3ebaeff1c
flaraloza97 <flaraloza97@penguin>
2020-09-25 10:53:59 -0500

    add advanced task

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints out the highest prime number
+ * Return: 0
+ */
+int main(void)
+{
+  int i;
+  long n;
+
+  n = 612852475143;
+  i = 2;
+
+  while (i < n)
+    {
+      while (n % i == 0 && i !=n)
+	n /= i;
+      i++;
+    }
+  printf("%lu\n", n);
+  return (0);
+}
